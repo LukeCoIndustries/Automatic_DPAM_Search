726f069bb844594c7c106cc4438af865536ba9ab
Lukeco <Lukecoreseeach@gmail.com>
2024-09-20 19:43:31 -0400

    added the imported databases

--- Protein_Search.py
+++ Protein_Search.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 #################################################
-# Programming assignment #1
-#
-# Name: Luke Coster
-
+# Protein Search using ECOD's AlphaFold DataBase
+# Creater: Luke Coster
 ##################################################
-
-
+#This is the extra databases needed to be imported
+import pandas as pd
+import os
+import matplotlib.pyplot as ply
 ###################################################
