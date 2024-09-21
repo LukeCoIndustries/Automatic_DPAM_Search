c90b854ec1c6febe1f92a392af1f1d24245765ea
Lukeco <Lukecoreseeach@gmail.com>
2024-09-21 15:51:43 -0400

    adding areas whereone can put ther data

--- Protein_Search.py
+++ Protein_Search.py
@@ -8,3 +8,18 @@ import pandas as pd
 import os
 import matplotlib.pyplot as ply
 ###################################################
+#!!! ADD YOUR INFORMATIOM HERE !!!
+#--------------------------------------------------
+#add the file path for your proteomic data (Excel Sheet)
+your_data = ""
+
+#Whar column is your uniprot IDs in (in python Column 1 = 0, Column 2 = 1 , ect.)
+Uniprot_column = 0 #edit the number to your columns
+#Would you like a pie chart summary of 
+Pie_chart="yes"
+
+###################################################
+
+#This is opening the files for me adjust
+human_alpha_fold_class = pd.read_excel("")
+
