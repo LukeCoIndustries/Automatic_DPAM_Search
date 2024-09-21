43fae911eaf96b42e824ee282b017c90c30ad776
Lukeco <Lukecoreseeach@gmail.com>
2024-09-21 16:11:30 -0400

    adding files and destinations

--- Protein_Search.py
+++ Protein_Search.py
@@ -15,11 +15,15 @@ your_data = ""
 
 #Whar column is your uniprot IDs in (in python Column 1 = 0, Column 2 = 1 , ect.)
 Uniprot_column = 0 #edit the number to your columns
-#Would you like a pie chart summary of 
-Pie_chart="yes"
+#Would you like a pie chart summary of each subclass of domain structures (yes or no)
+Pie_chart="" 
 
 ###################################################
 
 #This is opening the files for me adjust
-human_alpha_fold_class = pd.read_excel("")
+human_alpha_fold_class_df = pd.read_excel("HomSa_raw_domains.xlsx") #Contains Uniprot ID's And ECOD domain classifications for AlphaFold Models
+ECOD_domain_dictionary_raw_df = pd.read_excel("ecod.latest.domains.xlsx") #ECOD domain classification ID Dictionary (contains duplicates and redundancies)
+your_data_df = pd.read_excel(your_data)
+
+
 
