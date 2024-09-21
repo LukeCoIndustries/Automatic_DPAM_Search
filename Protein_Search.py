05646d6a3e48ba34e5d1cac931b9eb9d48c352de
Lukeco <Lukecoreseeach@gmail.com>
2024-09-21 16:39:18 -0400

    adding protein count

--- Protein_Search.py
+++ Protein_Search.py
@@ -21,9 +21,26 @@ Pie_chart=""
 ###################################################
 
 #This is opening the files for me adjust
-human_alpha_fold_class_df = pd.read_excel("HomSa_raw_domains.xlsx") #Contains Uniprot ID's And ECOD domain classifications for AlphaFold Models
+human_alpha_fold_class_raw_df = pd.read_excel("HomSa_raw_domains.xlsx") #Contains Uniprot ID's And ECOD domain classifications for AlphaFold Models
 ECOD_domain_dictionary_raw_df = pd.read_excel("ecod.latest.domains.xlsx") #ECOD domain classification ID Dictionary (contains duplicates and redundancies)
 your_data_df = pd.read_excel(your_data)
+
+###################################################
+
+#Cleaning redundancies and duplicates or data that is not needed
+ECOD_domain_dictionary_df = ECOD_domain_dictionary_raw_df.drop(ECOD_domain_dictionary_raw_df.columns[0,1,2,4,5,6,7,8,13,14,15], axis = 1)
+ECOD_domain_dictionary_df.drop_duplicates(inplace = True)
+human_alpha_fold_class_df = human_alpha_fold_class_raw_df.drop(human_alpha_fold_class_raw_df.columns[1,2,4,5], axis = 1)
+human_alpha_fold_class_df.drop_duplicates(inplace= True)
+your_data_onlyUniProt_df = your_data_df.columns[Uniprot_column]
+
+###################################################
+
+#Counts how many proteins we have at the start
+total_protein = len(your_data_onlyUniProt_df)
+print("There are "+total_protein+" proteins")
+
+###################################################
 
 
 
